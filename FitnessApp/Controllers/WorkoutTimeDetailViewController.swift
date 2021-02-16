459d6ddf091f29d1287e56c43ed7e531dcb60ca2
Alex Vozniuk <alexvznk@gmail.com>
2021-02-16 14:29:32 -0800

    separate view from vc

--- FitnessApp/Controllers/WorkoutTimeDetailViewController.swift
+++ FitnessApp/Controllers/WorkoutTimeDetailViewController.swift
@@ -14,55 +14,20 @@ protocol WorkoutTimeDetailDelegate: class {
 
 class WorkoutTimeDetailViewController: UIViewController {
     
-    //MARK: - UI
-    
-    private let workoutNameTitleLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "Name:"
-        lbl.font = .systemFont(ofSize: 17, weight: .bold)
-        return lbl
-    }()
-    
-    private let workoutNameValueLabel = UILabel()
-    
-    private let workoutDurationTitleLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "Duration (min):"
-        lbl.font = .systemFont(ofSize: 17, weight: .bold)
-        return lbl
-    }()
-    
-    private let workoutDurationTextField: UITextField = {
-        let tf = UITextField()
-        tf.borderStyle = .roundedRect
-        tf.textAlignment = .center
-        tf.keyboardType = .decimalPad
-        tf.snp.makeConstraints { (make) in
-            make.width.equalTo(100)
-        }
-        return tf
-    }()
-    
-    private let containerStackView: UIStackView = {
-        let sv = UIStackView()
-        sv.axis = .vertical
-        sv.alignment = .center
-        sv.distribution = .equalSpacing
-        sv.spacing = 10
-        return sv
-    }()
-    
-    //MARK: - Variables
-    
     private var workout: Workout
+    private let rootView = WorkoutTimeDetailView()
     weak var delegate: WorkoutTimeDetailDelegate?
+    
+    override func loadView() {
+        self.view = rootView
+    }
     
     //MARK: - Init
     
     init(workout: Workout) {
         self.workout = workout
-        workoutNameValueLabel.text = workout.workoutName
-        workoutDurationTextField.text = workout.workoutDuration
+        rootView.workoutNameValueLabel.text = workout.workoutName
+        rootView.workoutDurationTextField.text = workout.workoutDuration
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -70,33 +35,14 @@ class WorkoutTimeDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
+        navigationItem.title = "Workout details"
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        guard let duration = workoutDurationTextField.text else { return }
+        guard let duration = rootView.workoutDurationTextField.text else { return }
         workout.workoutDuration = duration
         delegate?.workoutDidUpdateDuration(workout: workout)
-    }
-    
-    //MARK: - Initial UI Configuration
-    
-    private func configureUI() {
-        view.backgroundColor = .white
-        navigationItem.title = "Workout details"
-        
-        containerStackView.addArrangedSubview(workoutNameTitleLabel)
-        containerStackView.addArrangedSubview(workoutNameValueLabel)
-        containerStackView.addArrangedSubview(workoutDurationTitleLabel)
-        containerStackView.addArrangedSubview(workoutDurationTextField)
-        
-        view.addSubview(containerStackView)
-        
-        containerStackView.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide).inset(40)
-        }
     }
     
     required init?(coder: NSCoder) {
